e7d44b79f12d1e320b0e94a9a818e93958f7cac9
temp <143986725+tempservice@users.noreply.github.com>
2024-04-06 22:03:52 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -33,81 +33,30 @@ local ServerHop = function()
 		queue_on_teleport(queue)
 	end
 
-	local PlaceID = game.PlaceId
-	local AllIDs = {}
-	local foundAnything = ""
-	local actualHour = os.date("!*t").hour
-	local Deleted = false
-	local File = pcall(function()
-		AllIDs = game:GetService('HttpService'):JSONDecode(readfile("NotSameServers.json"))
-	end)
-	if not File then
-		table.insert(AllIDs, actualHour)
-		writefile("NotSameServers.json", game:GetService('HttpService'):JSONEncode(AllIDs))
-	end
-	function TPReturner()
-		local Site;
-		if foundAnything == "" then
-			Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100'))
-		else
-			Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100&cursor=' .. foundAnything))
-		end
-		local ID = ""
-		if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
-			foundAnything = Site.nextPageCursor
-		end
-		local num = 0;
-		for i,v in pairs(Site.data) do
-			local Possible = true
-			ID = tostring(v.id)
-			if tonumber(v.playing) <= 5 then
-				for _,Existing in pairs(AllIDs) do
-					if num ~= 0 then
-						if ID == tostring(Existing) then
-							Possible = false
-						end
-					else
-						if tonumber(actualHour) ~= tonumber(Existing) then
-							local delFile = pcall(function()
-								delfile("NotSameServers.json")
-								AllIDs = {}
-								table.insert(AllIDs, actualHour)
-							end)
-						end
-					end
-					num = num + 1
-				end
-				if Possible == true then
-					table.insert(AllIDs, ID)
-					wait()
-					pcall(function()
-						writefile("NotSameServers.json", game:GetService('HttpService'):JSONEncode(AllIDs))
-						wait()
-						game:GetService("TeleportService"):TeleportToPlaceInstance(PlaceID, ID, game.Players.LocalPlayer)
-					end)
-					wait(2.25)
-				end
-			end
-		end
+	local Http = game:GetService("HttpService")
+	local TPS = game:GetService("TeleportService")
+	local Api = "https://games.roblox.com/v1/games/"
+
+	local _place = game.PlaceId
+	local _servers = Api.._place.."/servers/Public?sortOrder=Asc&limit=100"
+
+	function ListServers(cursor)
+		local Raw = game:HttpGet(_servers .. ((cursor and "&cursor="..cursor) or ""))
+		return Http:JSONDecode(Raw)
 	end
 
-	function TeleportSmall()
-		while wait() do
-			pcall(function()
-				TPReturner()
-				if foundAnything ~= "" then
-					TPReturner()
-				end
-			end)
-		end
-	end
+	local Server, Next; repeat
+		local Servers = ListServers(Next)
+		Server = Servers.data[1]
+		Next = Servers.nextPageCursor
+	until Server
 
 	if pcall(function()
-			TeleportSmall()
-		end) then ServerHopping = true return end
+			TPS:TeleportToPlaceInstance(_place,Server.id,game:GetService("Players").LocalPlayer)				
+		end) and not game:GetService("Players").LocalPlayer then ServerHopping = true return end
 end
 
-for i = 0, 1 do
+for i = 1, 1 do
 	SpamChat()
 end
 
